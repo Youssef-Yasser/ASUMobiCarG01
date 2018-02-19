ffa9fd78a40a1e596bf0556659c5ccd832cd1960
youssefmoro <36382324+youssefmoro@users.noreply.github.com>
2018-02-19 23:39:48 +0200

    Add files via upload
     ledpins lighting in sequence

---
+++ Moro/q2_sheet_1.ino
@@ -0,0 +1,36 @@
+
+int ledpin4 = 4 , ledpin5 = 5 , ledpin6 = 6 , ledpin7 = 7 ;
+
+
+void setup() {
+pinMode (ledpin4 , OUTPUT ) ; 
+pinMode (ledpin5 , OUTPUT ); 
+pinMode (ledpin6 , OUTPUT ); 
+pinMode (ledpin7 , OUTPUT ); 
+ 
+}
+
+
+void loop() {
+ 
+ digitalWrite (ledpin4 , HIGH);
+ delay (1000);
+  
+  digitalWrite (ledpin4 ,LOW );
+  digitalWrite (ledpin5 , HIGH);
+
+    delay (1000);
+   
+   digitalWrite (ledpin5 ,LOW );
+   digitalWrite (ledpin6 , HIGH);
+
+   delay (1000);
+   
+    digitalWrite (ledpin6 ,LOW );
+    digitalWrite (ledpin7 , HIGH);
+    
+    delay (1000);
+    
+      digitalWrite (ledpin7 ,LOW );
+
+}
