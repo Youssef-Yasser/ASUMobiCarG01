e00738f91843a3b33d5a2d82e001e068923c2ecd
youssefmoro <yousefmoro97@gmail.com>
2018-02-19 05:48:25 -0800

    adding on off led q1 sheet1

---
+++ Moro/on_off__led/on_off__led.ino
@@ -0,0 +1,15 @@
+int LED = 5 ;
+
+void setup() {
+  
+  // put your setup code here, to run once:
+pinMode(LED , OUTPUT) ;
+}
+
+void loop() {
+  digitalWrite (LED , HIGH);
+  delay (500);
+  digitalWrite (LED , LOW);
+  delay (500); 
+
+}
