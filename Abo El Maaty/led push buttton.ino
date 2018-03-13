14db4527068da2f8f348a5d4aad8f3e3aa18ca7b
yousefmohamed23 <36644002+yousefmohamed23@users.noreply.github.com>
2018-03-13 21:58:53 +0300

    Add files via upload

---
+++ Abo El Maaty/led push buttton.ino
@@ -0,0 +1,26 @@
+const int buttonpin = 2 ;
+const int ledpin=13;
+int buttonstate = 0;
+void setup ()
+{
+  pinMode ( buttonpin , INPUT );
+  pinMode (ledpin , OUTPUT );
+}
+void loop ()
+{
+  buttonstate = digitalRead ( buttonpin);
+  if ( buttonstate == HIGH )
+  {
+    digitalWrite ( ledpin , HIGH );
+    
+  }else 
+  {
+    digitalWrite (ledpin ,LOW) ;
+    
+  }
+  
+  
+
+
+
+}
