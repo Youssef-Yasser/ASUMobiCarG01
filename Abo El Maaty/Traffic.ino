725771c2136c7320a3dcb23c34637d3ec5315619
yousefmohamed23 <36644002+yousefmohamed23@users.noreply.github.com>
2018-03-13 22:21:29 +0300

    Add files via upload

---
+++ Abo El Maaty/Traffic.ino
@@ -0,0 +1,24 @@
+int yellow=13 , red =12 , green=11 ;
+void setup ()
+{
+  
+   pinMode(red , OUTPUT);
+   pinMode(yellow ,OUTPUT);
+   pinMode(green ,OUTPUT);
+}
+void loop ()
+{
+  digitalWrite(yellow,HIGH);
+  digitalWrite(green,LOW);
+  digitalWrite(red,LOW);
+  delay (3000);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,HIGH);
+  digitalWrite(red,LOW);
+  delay(3000);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,LOW);
+  digitalWrite(red,HIGH);
+  delay(3000);
+  
+}
