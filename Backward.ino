387f2e99d2b81ab448b0341bb9c4bb572ba08d9c
MostafaMohamedAli <36517625+MostafaMohamedAli@users.noreply.github.com>
2018-05-29 13:07:33 +0200

    functions
    Backward Function

---
+++ Backward.ino
@@ -0,0 +1,36 @@
+#define m11 11 // M1a
+#define m12 12 // M1b
+
+#define m21 10 // M2a
+#define m22 9 // M2b
+char data;
+
+void backward()
+{
+digitalWrite(m11, LOW);
+digitalWrite(m12, HIGH);
+digitalWrite(m21, LOW);
+digitalWrite(m22, HIGH);
+}
+
+void setup() {
+   // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(m11, OUTPUT);
+pinMode(m12, OUTPUT);
+pinMode(m21, OUTPUT);
+pinMode(m22, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while(Serial.available())
+{
+  if(data =='B') //backward
+{
+Serial.println("backward");
+backward();
+
+}
+  }
+}
