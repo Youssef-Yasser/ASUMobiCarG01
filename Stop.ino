a0c5fe4497c82f9bd631b2bb60eef80cf06556eb
MostafaMohamedAli <36517625+MostafaMohamedAli@users.noreply.github.com>
2018-05-29 13:10:31 +0200

    Functions
    Stop

---
+++ Stop.ino
@@ -0,0 +1,13 @@
+void setup()
+{  
+    pinMode(13 ,OUTPUT);
+    pinMode(2 ,INPUT);   
+}
+void loop ()
+{  
+   if (digitalRead(2) == 1)
+       digitalWrite( 13, HIGH); 
+   else
+       digitalWrite( 13, LOW);  
+}
+
